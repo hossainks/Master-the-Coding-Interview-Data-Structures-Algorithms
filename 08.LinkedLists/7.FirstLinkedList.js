9042128cb7f2e0cdeb71a7ef2463bad309aa8d01
hossainks <shakirhossain.qa@gmail.com>
2025-05-28 22:01:03 +0600

    Linked list

--- 08.LinkedLists/7.FirstLinkedList.js
+++ 08.LinkedLists/7.FirstLinkedList.js
@@ -1,18 +1,3 @@
-// 10->5->16->20->25
-
-// let myLinkedList = {
-//   head: {
-//     value: 10,
-//     next: {
-//       value: 5,
-//       next: {
-//         value: 16,
-//         next: null,
-//       },
-//     },
-//   },
-// };
-
 class LinkedList {
   constructor(value) {
     this.head = {
@@ -41,6 +26,74 @@ class LinkedList {
     this.length++;
     return this;
   }
+
+  printList() {
+    let currentNode = this.head;
+    const nodesValue = [];
+    while (currentNode !== null) {
+      nodesValue.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    return nodesValue;
+  }
+
+  insert(index, value) {
+    if (index >= this.length) {
+      return append(value);
+    }
+    const newNode = this.createNode(value);
+    const leader = this.traverseToIndex(index);
+    const holdingPointer = leader.next;
+    leader.next = newNode;
+    newNode.next = holdingPointer;
+    this.length++;
+
+    this.printList();
+  }
+
+  remove(index) {
+    // [25, 10, 63, 5, 16];
+    const leader = this.traverseToIndex(index);
+    const unwantedNode = leader.next;
+    leader.next = unwantedNode.next;
+    this.length--;
+
+    return this.printList();
+  }
+
+  traverseToIndex(index) {
+    let counter = 0;
+    let currentNode = this.head;
+    while (counter !== index - 1) {
+      // Stop at predecessor
+      currentNode = currentNode.next;
+      counter++;
+    }
+    return currentNode; // Returns the leader
+  }
+  /*   insert(index, value) {
+    let currentNode = this.head;
+    let counter = 0;
+    const data = [];
+    // [ 25, 10, 5, 16 ]
+    for (let i = 0; i < this.length; i++) {
+      if (currentNode.value !== undefined) {
+        if (counter === index - 1) {
+          const newNode = this.createNode(value);
+          const holdingNode = currentNode.next;
+          newNode.next = holdingNode;
+          currentNode.next = newNode;
+          this.length++;
+          break;
+        }
+
+        data.push(currentNode.value);
+        currentNode = currentNode.next;
+        counter++;
+      }
+    }
+    return { data, counter };
+  } */
 }
 let myLinkedList = new LinkedList(10);
 console.log(myLinkedList);
@@ -48,3 +101,9 @@ myLinkedList.append(5);
 myLinkedList.append(16);
 myLinkedList.prepend(25);
 console.dir(myLinkedList, { depth: null, color: true });
+console.log('====================');
+console.log(myLinkedList.insert(2, 63));
+console.dir(myLinkedList.printList());
+console.log('====================');
+console.log(myLinkedList.remove(2));
+console.log(myLinkedList.remove(2));
